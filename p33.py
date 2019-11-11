d6bdabaa9413c1ac8f15f9d258be2015dbb866c3
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 04:36:16 +0600

    Update p33.py

--- p33.py
+++ p33.py
@@ -1,6 +1,7 @@
+# For a given integer N, print all the squares of integer numbers where the
+# square is less than or equal to N, in ascending order.
 a=int(input("Enter your number:"))
 i=1
 while i**2<=a**2:
 	print(i**2) 
 	i += 1
-
